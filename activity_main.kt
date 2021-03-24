e43e7f56c8f234ef1d7a60916f2959ea52cf7d86
Gaurav200115 <76817030+Gaurav200115@users.noreply.github.com>
2021-03-24 10:38:00 +0530

    activity_main.kt

---
+++ activity_main.kt
@@ -0,0 +1,21 @@
+import android.os.Build
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.google.android.material.bottomnavigation.BottomNavigationViewclass
+
+MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+      
+        val bottom = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navController = findNavController(R.id.fragment)
+        bottom.background = null
+        bottom.setupWithNavController(navController)
+
+    }
